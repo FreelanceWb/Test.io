6f7897d947552e70eba534f7e058e0effbc1c47b
tomatosoooup <you@example.com>
2022-09-20 18:21:56 +0300

    added slide with services/adaptive

--- Company/js/script.js
+++ Company/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $(".arrow-container").on("click", "a", function (event) {
+  $(".arrow-container, #page-links").on("click", "a", function (event) {
     event.preventDefault();
     let id = $(this).attr("href");
     let top = $(id).offset().top;
@@ -13,5 +13,29 @@ $(document).ready(function () {
     let value = window.scrollY;
     navbar.style.top = value * 0.2 + "px";
     devs.style.left = value * 0.2 + "px";
+  });
+
+  $("#page-links li a").on("click", function () {
+    const page_links = document.querySelectorAll("#page-links li a");
+    page_links.forEach((link) => {
+      link.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+
+  $("#portfolio span").on("click", function () {
+    const portfolio_spans = document.querySelectorAll("#portfolio span");
+    portfolio_spans.forEach((span) => {
+      span.classList.remove("active");
+    });
+    this.classList.add("active");
+  });
+
+  $(".service-block").on("click", function () {
+    const service_blocks = document.querySelectorAll(".service-block");
+    service_blocks.forEach((block) => {
+      block.classList.remove("active");
+    });
+    this.classList.add("active");
   });
 });
