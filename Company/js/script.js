cfa168b5c7d0a634a14792df673062cfd7cfbfc1
tomatosoooup <you@example.com>
2022-09-20 16:00:17 +0300

    tested arrow slide

--- Company/js/script.js
+++ Company/js/script.js
@@ -1,15 +1,17 @@
-// let bg = document.getElementById("bg");
-// let bg_blue = document.getElementById("bg-blue");
+$(document).ready(function () {
+  $(".arrow-container").on("click", "a", function (event) {
+    event.preventDefault();
+    let id = $(this).attr("href");
+    let top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
 
-// window.addEventListener("scroll", function () {
-//   let value = window.scrollY;
-// bg.style.top = value * 0.8 + "px";
-// bg_blue.style.top = value * 0.1 + "px";
-// });
+  let navbar = document.getElementById("z-index-navbar");
+  let devs = document.getElementById("devs");
 
-// $(".sec, .section").on("click", "a", function (event) {
-//   event.preventDefault();
-//   let id = $(this).attr("href");
-//   let top = $(id).offset().top;
-//   $("body,html").animate({ scrollTop: top }, 1500);
-// });
+  window.addEventListener("scroll", function () {
+    let value = window.scrollY;
+    navbar.style.top = value * 0.2 + "px";
+    devs.style.left = value * 0.2 + "px";
+  });
+});
