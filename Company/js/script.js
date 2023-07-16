39c2c813e8177d490d44afad1dd7e964ac482d0c
Vasyl <94603459+tomatosoooup@users.noreply.github.com>
2023-07-16 14:36:04 +0300

    Update script.js

--- Company/js/script.js
+++ Company/js/script.js
@@ -1,17 +1,10 @@
 $(document).ready(function () {
-  $(".arrow-container, #page-links, .footer-text").on(
-    "click",
-    "a",
-    function (event) {
-      event.preventDefault();
-      let id = $(this).attr("href");
-      let top = $(id).offset().top;
-      $("body,html").animate({ scrollTop: top }, 1500);
-      if (document.querySelector(".ham").classList.contains("active")) {
-        document.querySelector(".navbar-toggler").click();
-      }
-    }
-  );
+  $("a").on("click", function (event) {
+    event.preventDefault();
+    let id = $(this).attr("href");
+    let top = $(id).offset().top;
+    $("body, html").animate({ scrollTop: top }, 800);
+  });
 
   $("#page-links li a").on("click", function () {
     const page_links = document.querySelectorAll("#page-links li a");
@@ -21,11 +14,52 @@ $(document).ready(function () {
     this.classList.add("active");
   });
 
-  $("#portfolio span").on("click", function () {
-    const portfolio_spans = document.querySelectorAll("#portfolio span");
-    portfolio_spans.forEach((span) => {
-      span.classList.remove("active");
-    });
-    this.classList.add("active");
+  function handleDragStart(e) {
+    this.style.opacity = "0.4";
+
+    dragSrcEl = this;
+
+    e.dataTransfer.effectAllowed = "move";
+    e.dataTransfer.setData("text/html", this.innerHTML);
+  }
+
+  function handleDragOver(e) {
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+
+    return false;
+  }
+
+  function handleDragEnter(e) {
+    this.classList.add("over");
+  }
+
+  function handleDragLeave(e) {
+    this.classList.remove("over");
+  }
+
+  function handleDragEnd(e) {
+    this.style.opacity = "1";
+  }
+
+  function handleDrop(e) {
+    e.stopPropagation();
+    if (dragSrcEl !== this) {
+      dragSrcEl.innerHTML = this.innerHTML;
+      this.innerHTML = e.dataTransfer.getData("text/html");
+    }
+
+    return false;
+  }
+
+  let items = document.querySelectorAll(".skill-block");
+  items.forEach(function (item) {
+    item.addEventListener("dragstart", handleDragStart);
+    item.addEventListener("dragover", handleDragOver);
+    item.addEventListener("dragenter", handleDragEnter);
+    item.addEventListener("dragleave", handleDragLeave);
+    item.addEventListener("dragend", handleDragEnd);
+    item.addEventListener("drop", handleDrop);
   });
 });
