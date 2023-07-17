669094c0086785ff9678fdc86b4c617824f60c6d
Vasyl <v.gabinet02@icloud.com>
2023-07-17 11:53:43 +0300

    testing cdn jquery w burger menu

--- Company/js/script.js
+++ Company/js/script.js
@@ -1,65 +1,68 @@
-$(document).ready(function () {
-  $("a").on("click", function (event) {
-    event.preventDefault();
-    let id = $(this).attr("href");
-    let top = $(id).offset().top;
-    $("body, html").animate({ scrollTop: top }, 800);
-  });
-
-  $("#page-links li a").on("click", function () {
-    const page_links = document.querySelectorAll("#page-links li a");
-    page_links.forEach((link) => {
-      link.classList.remove("active");
-    });
-    this.classList.add("active");
-  });
-
-  function handleDragStart(e) {
-    this.style.opacity = "0.4";
-
-    dragSrcEl = this;
-
-    e.dataTransfer.effectAllowed = "move";
-    e.dataTransfer.setData("text/html", this.innerHTML);
-  }
-
-  function handleDragOver(e) {
-    if (e.preventDefault) {
-      e.preventDefault();
-    }
-
-    return false;
-  }
-
-  function handleDragEnter(e) {
-    this.classList.add("over");
-  }
-
-  function handleDragLeave(e) {
-    this.classList.remove("over");
-  }
-
-  function handleDragEnd(e) {
-    this.style.opacity = "1";
-  }
-
-  function handleDrop(e) {
-    e.stopPropagation();
-    if (dragSrcEl !== this) {
-      dragSrcEl.innerHTML = this.innerHTML;
-      this.innerHTML = e.dataTransfer.getData("text/html");
-    }
-
-    return false;
-  }
-
-  let items = document.querySelectorAll(".skill-block");
-  items.forEach(function (item) {
-    item.addEventListener("dragstart", handleDragStart);
-    item.addEventListener("dragover", handleDragOver);
-    item.addEventListener("dragenter", handleDragEnter);
-    item.addEventListener("dragleave", handleDragLeave);
-    item.addEventListener("dragend", handleDragEnd);
-    item.addEventListener("drop", handleDrop);
-  });
+$(".nav-item a, .arrow-link").on("click", function (event) {
+  event.preventDefault();
+  let id = $(this).attr("href");
+  let top = $(id).offset().top;
+  $("body, html").animate({ scrollTop: top }, 500);
 });
+
+// $("#page-links li a").on("click", function () {
+//   const page_links = document.querySelectorAll("#page-links li a");
+//   page_links.forEach((link) => {
+//     link.classList.remove("active");
+//   });
+//   this.classList.add("active");
+// });
+
+$(".burger-button").on("click", function () {
+  $("#canvas").toggleClass("show");
+  console.log($("#canvas"));
+});
+
+// function handleDragStart(e) {
+//   this.style.opacity = "0.4";
+
+//   dragSrcEl = this;
+
+//   e.dataTransfer.effectAllowed = "move";
+//   e.dataTransfer.setData("text/html", this.innerHTML);
+// }
+
+// function handleDragOver(e) {
+//   if (e.preventDefault) {
+//     e.preventDefault();
+//   }
+
+//   return false;
+// }
+
+// function handleDragEnter(e) {
+//   this.classList.add("over");
+// }
+
+// function handleDragLeave(e) {
+//   this.classList.remove("over");
+// }
+
+// function handleDragEnd(e) {
+//   this.style.opacity = "1";
+// }
+
+// function handleDrop(e) {
+//   e.stopPropagation();
+//   if (dragSrcEl !== this) {
+//     dragSrcEl.innerHTML = this.innerHTML;
+//     this.innerHTML = e.dataTransfer.getData("text/html");
+//   }
+
+//   return false;
+// }
+
+// let items = document.querySelectorAll(".skill-block");
+// items.forEach(function (item) {
+//   item.addEventListener("dragstart", handleDragStart);
+//   item.addEventListener("dragover", handleDragOver);
+//   item.addEventListener("dragenter", handleDragEnter);
+//   item.addEventListener("dragleave", handleDragLeave);
+//   item.addEventListener("dragend", handleDragEnd);
+//   item.addEventListener("drop", handleDrop);
+// });
